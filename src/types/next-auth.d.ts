42c84a5783e4d71fbfd5fad8081e395818041062
alekskuzminov <webdev.kuzminov@gmail.com>
2026-05-07 09:19:18 +0300

    правки

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,17 @@
 import 'next-auth';
 import 'next-auth/jwt';
+import type { Role } from '@prisma/client';
 
 declare module 'next-auth' {
     interface User {
-        role: string;
+        role: Role;
     }
     interface Session {
         user: {
             id: string;
             email: string;
             name: string;
-            role: string;
+            role: Role;
         };
     }
 }
@@ -18,6 +19,6 @@ declare module 'next-auth' {
 declare module 'next-auth/jwt' {
     interface JWT {
         id: string;
-        role: string;
+        role: Role;
     }
 }
